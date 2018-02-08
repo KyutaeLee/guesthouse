642173d6be9c774f83e4847dafd038b70a5d836b
kyutaelee <rbxofjqn@gmail.com>
2018-02-08 16:58:44 +0900

    1day 1commit

---
+++ sitepage/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def hi(request):
+    return HttpResponse("Hello")
+
+def home(request):
+    return render(request, 'sitepage/home.html', {})
+
+def about(request):
+    return render(request, 'sitepage/about.html', {})
